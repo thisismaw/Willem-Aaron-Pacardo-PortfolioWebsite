464f7b7e6425fc9cbccf3f685799e8b7330c51cf
Willem Aaron G. Pacardo <iamwillempacardo@gmail.com>
2025-05-29 21:53:01 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
     unoptimized: true, // Disable Next.js Image Optimization for static export
   },
    basePath: '/Willem-Aaron-Pacardo-PortfolioWebsite', 
+   assetPrefix: '/Willem-Aaron-Pacardo-PortfolioWebsite',
   // Make sure this is your actual repository name if you uncomment it.
   // For example, if your GitHub Pages URL will be https://iAMawi.github.io/willem-pacardo-portfolio/
   // then basePath should be '/willem-pacardo-portfolio'
