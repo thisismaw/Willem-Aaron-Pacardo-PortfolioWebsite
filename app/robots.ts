90d11b8e0c4c4025616beeaf97f87614e6708cee
Willem Aaron G. Pacardo <iamwillempacardo@gmail.com>
2026-01-13 17:01:25 +0800

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.MetadataRoute {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://willem-aaron-pacardo-portfolio-webs.vercel.app/sitemap.xml',
+  }
+}
