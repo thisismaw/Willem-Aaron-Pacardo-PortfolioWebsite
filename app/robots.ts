d48d1e871c976be1f5cff1a0b543abbc10644a05
Willem Aaron G. Pacardo <iamwillempacardo@gmail.com>
2026-01-13 17:06:38 +0800

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
  
-export default function robots(): MetadataRoute.MetadataRoute {
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
